a2fa9a4988d0018279d5ebc7ddd494d8e98cd56c
Mahin-Hasan <mahinmtrs@gmail.com>
2024-11-21 13:04:52 +0600

    created module,interface, service and controller for Tour schema

---
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/tour-and-travel-server/src/app/modules/tour/tour.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+const tourSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  durationHours: {
+    type: Number,
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    default: 5,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  coverImage: { type: String, required: true },
+  images: [String],
+  startDates: [Date],
+  startLocation: { type: String },
+  locations: [String], //array of string when declared in mongoose
+  slug: String,
+});
+
+const Tour = model('Tour', tourSchema);
+
+export default Tour;
--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/tour-and-travel-server/src/app/modules/user/user.model.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/tour-and-travel-server/src/app/modules/user/user.model.ts
@@ -19,7 +19,7 @@ const userSchema = new Schema<IUser>({
       },
       message: '{VALUE} is not a valid email',
     },
-    immutable:true,// email er value asholeo update korbe na part 5 23min
+    immutable: true, // email er value asholeo update korbe na part 5 23min
   },
   photo: String,
   role: {
@@ -28,13 +28,25 @@ const userSchema = new Schema<IUser>({
       values: ['user', 'admin'],
       message: '{VALUE} is not a valid. please provide a valid user',
     },
-    default: 'user', //setting role as user by default 
+    default: 'user', //setting role as user by default
     required: true,
   },
   userStatus: { type: String, enum: ['active', 'inactive'], required: true },
 });
 
-//pre hook
+//pre hook to get only active users
+// userSchema.pre('find', function (this, next) {
+//   this.find({ userStatus: { $eq: 'active' } }); // here this works as a model
+//   next();
+// });
+//post hook to show name as uppercase
+userSchema.post('find', function (docs, next) {
+  // console.log(docs);
+  docs.forEach((doc:IUser) =>{
+    doc.name= doc.name.toUpperCase()
+  })  //foreach bz array of obj
+  next();
+});
 
 const User = model<IUser>('User', userSchema);
 export default User;
