06239a2cee0f763960fa5a20d66a7db8f8b29568
Mahin-Hasan <mahinmtrs@gmail.com>
2024-12-08 00:14:17 +0600

    created Academic faculty routes, controllers and services

--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,13 +1,17 @@
 import { z } from 'zod';
 
-const AcademicFacultyValidatoinSchema = z.object({
-  //   id: z.string(), auto generate
-  name: z
-    .string({
-      invalid_type_error: 'Academic Faculty must be string',
-    })
+const createAcademicFacultyValidatoinSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Faculty must be string',
+  }),
+});
+const updateAcademicFacultyValidatoinSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Faculty must be string',
+  }),
 });
 
-export const UserValidation = {
-  AcademicFacultyValidatoinSchema,
+export const AcademicFacultyValidations = {
+  createAcademicFacultyValidatoinSchema,
+  updateAcademicFacultyValidatoinSchema,
 };
