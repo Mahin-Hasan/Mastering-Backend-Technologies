99e520574b5e12ff3caba61f40ce03d8ca19eb87
Mahin-Hasan <mahinmtrs@gmail.com>
2024-12-14 21:29:35 +0600

    created offered course interface, model, constant and validation

---
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from 'mongoose';
+
+export type TDays = 'Sat' | 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+
+// referencing multiple fields so that we can search faster and we also want to update fronend data based on user selected fields
+export type TOfferedCourse = {
+  semesterRegistration: Types.ObjectId;
+  academicSemester?: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  days: TDays[]; // enum
+  startTime: string;
+  endTime: string;
+};
---
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+import { Days } from './offeredCourse.constant';
+
+const createOfferedCourseValidationSchema = z.object({
+  body: z.object({
+    semesterRegistration: z.string(),
+    academicSemester: z.string(),
+    academicFaculty: z.string(),
+    academicDepartment: z.string(),
+    course: z.string(),
+    faculty: z.string(),
+    section: z.number(),
+    maxCapacity: z.number(),
+    days: z.array(z.enum([...Days] as [string, ...string[]])),
+    startTime: z.string(), // HH: MM   00-23: 00-59
+    endTime: z.string(),
+  }),
+});
+const updateOfferedCourseValidationSchema = z.object({
+  body: z.object({
+    faculty: z.string().optional(),
+    maxCapacity: z.number().optional(),
+    days: z.array(z.enum([...Days] as [string, ...string[]])).optional(),
+    startTime: z.string().optional(), // HH: MM   00-23: 00-5.9
+    endTime: z.string().optional(),
+  }),
+});
+
+export const OfferedCourseValidations = {
+  createOfferedCourseValidationSchema,
+  updateOfferedCourseValidationSchema,
+};
