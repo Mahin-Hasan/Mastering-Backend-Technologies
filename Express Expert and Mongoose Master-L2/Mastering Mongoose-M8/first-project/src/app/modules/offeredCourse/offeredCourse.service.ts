425bb8422b272c70fe8000dc5bf0610049e67401
Mahin-Hasan <mahinmtrs@gmail.com>
2024-12-15 04:04:38 +0600

    validated start and end hour timing check, validated faculty-department, same course section

--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.service.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -16,6 +16,7 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     academicDepartment,
     course,
     faculty,
+    section,
   } = payload;
   // as we are not sending academicSemester from frontEnd we will need to check if the semester registration id isExist
   const isSemesterRegistrationExists =
@@ -51,6 +52,38 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
   }
 
+  /*
+  1. academic faculty er moddhe onek gula academin department thake | faculty of engineering -> CSE, EEE
+  */
+  // validation | check if created Academic faculty exist inside AcademicDepartment model || there must be a realtion between academicFaculty and AcademicDepartment
+  // Check if the department belongs to the faculty
+
+  const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
+    _id:academicDepartment, // check academicDepartment matches with payload id
+    academicFaculty,
+  }); // matching both model id
+
+  if (!isDepartmentBelongToFaculty) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This ${isAcademicDepartmentExists.name} is not belong to this ${isAcademicFacultyExists.name}`,
+    );
+  }
+
+  // check if the same offered course same section is in same regestered semester exists
+  const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection =
+    await OfferedCourse.findOne({
+      semesterRegistration,
+      course,
+      section,
+    });
+
+  if (isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `Offered course with same section is already exist!`,
+    );
+  }
   const result = await OfferedCourse.create({ ...payload, academicSemester }); // need to pass retrived academicSemester inside {}
   return result;
 };
--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,26 +1,61 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+// start time must be smaller than end time i.e 9-11 | both startTime and endTime are inside body so we can use refine in body
 const createOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    section: z.number(),
-    maxCapacity: z.number(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])), // must encapsulate inside array
-    startTime: z.string(), // HH: MM   00-23: 00-59
-    endTime: z.string(),
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      section: z.number(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])), // must encapsulate inside array
+      startTime: z.string().refine(
+        (time) => {
+          // console.log(time); // will show send time
+          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23 //regex to format time
+          return regex.test(time);
+        },
+        {
+          message: 'Invalid time format , expected "HH:MM" in 24 hours format',
+        },
+      ), // HH: MM   00-23: 00-59 | validation format
+      endTime: z.string().refine(
+        (time) => {
+          // console.log(time); // will show send time
+          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23 //regex to format time
+          return regex.test(time);
+        },
+        {
+          message: 'Invalid time format , expected "HH:MM" in 24 hours format',
+        },
+      ),
+    })
+    .refine(
+      (body) => {
+        //only comparing with
+        // startTime : 10:30  => 1970-01-01T10:30
+        //endTime : 12:30  =>  1970-01-01T12:30
+
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+
+        return end > start;
+      },
+      {
+        message: 'Start time should be before End time !  ',
+      },
+    ),
 });
 const updateOfferedCourseValidationSchema = z.object({
   body: z.object({
     faculty: z.string().optional(),
     maxCapacity: z.number().optional(),
     days: z.array(z.enum([...Days] as [string, ...string[]])).optional(),
-    startTime: z.string().optional(), // HH: MM   00-23: 00-5.9
+    startTime: z.string().optional(), // HH: MM   00-23: 00-59
     endTime: z.string().optional(),
   }),
 });
