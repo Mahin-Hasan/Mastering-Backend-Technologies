cce5fdf9d310b3f58efae5eec524699a3849461f
Mahin-Hasan <mahinmtrs@gmail.com>
2024-12-16 04:03:18 +0600

    implemented get all, set single, delete and update functionality

--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.service.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -9,6 +9,7 @@ import { AcademicDepartment } from '../academicDepartment/academicDepartment.mod
 import { Course } from '../course/course.model';
 import { Faculty } from '../faculty/faculty.model';
 import { hasTimeConflict } from './offeredCourse.utils';
+import QueryBuilder from '../../builder/QueryBuilder';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
   const {
@@ -22,6 +23,19 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     startTime,
     endTime,
   } = payload;
+  /*
+   * Step 1: check if the semester registration id is exists!
+   * Step 2: check if the academic faculty id is exists!
+   * Step 3: check if the academic department id is exists!
+   * Step 4: check if the course id is exists!
+   * Step 5: check if the faculty id is exists!
+   * Step 6: check if the department is belong to the  faculty
+   * Step 7: check if the same offered course same section in same registered semester exists
+   * Step 8: get the schedules of the faculties
+   * Step 9: check if the faculty is available at that time. If not then throw error
+   * Step 10: create the offered course
+   */
+
   // as we are not sending academicSemester from frontEnd we will need to check if the semester registration id isExist
   const isSemesterRegistrationExists =
     await SemesterRegistration.findById(semesterRegistration);
@@ -116,18 +130,122 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
     );
   }
 
-  // const result = await OfferedCourse.create({ ...payload, academicSemester }); // need to pass retrived academicSemester inside {}
-  // return result;
-  return null;
+  const result = await OfferedCourse.create({ ...payload, academicSemester }); // need to pass retrived academicSemester inside {}
+  return result;
 };
 
-const getAllOfferedCoursesFromDB = async (query) => {};
+const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {
+  const offeredCourseQuery = new QueryBuilder(OfferedCourse.find(), query)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-const getSingleOfferedCourseFromDB = async (id: string) => {};
+  const result = await offeredCourseQuery.modelQuery;
+  return result;
+};
+const getSingleOfferedCourseFromDB = async (id: string) => {
+  const offeredCourse = await OfferedCourse.findById(id);
 
-const updateOfferedCourseIntoDB = async (id: string, payload) => {};
+  if (!offeredCourse) {
+    throw new AppError(404, 'Offered Course not found');
+  }
 
-const deleteOfferedCourseFromDB = async (id: string) => {};
+  return offeredCourse;
+};
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Pick<TOfferedCourse, 'faculty' | 'days' | 'startTime' | 'endTime'>, // using partial will give error
+) => {
+  /*
+   * Step 1: check if the offered course exists
+   * Step 2: check if the faculty exists
+   * Step 3: check if the semester registration status is upcoming
+   * Step 4: check if the faculty is available at that time. If not then throw error
+   * Step 5: update the offered course
+   */
+  const { faculty, days, startTime, endTime } = payload;
+
+  const isOfferedCourseExists = await OfferedCourse.findById(id);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found !');
+  }
+
+  const isFacultyExists = await Faculty.findById(faculty);
+
+  if (!isFacultyExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
+  }
+
+  const semesterRegistration = isOfferedCourseExists.semesterRegistration;
+  // get the schedules of the faculties
+
+  // Checking the status of the semester registration
+  const semesterRegistrationStatus =
+    await SemesterRegistration.findById(semesterRegistration);
+
+  if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `You can not update this offered course as it is ${semesterRegistrationStatus?.status}`,
+    );
+  }
+
+  // check if the faculty is available at that time.
+  const assignedSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    faculty,
+    days: { $in: days },
+  }).select('days startTime endTime');
+
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+
+  if (hasTimeConflict(assignedSchedules, newSchedule)) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      `This faculty is not available at that time ! Choose other time or day`,
+    );
+  }
+
+  const result = await OfferedCourse.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteOfferedCourseFromDB = async (id: string) => {
+  /**
+   * Step 1: check if the offered course exists
+   * Step 2: check if the semester registration status is upcoming
+   * Step 3: delete the offered course
+   */
+  const isOfferedCourseExists = await OfferedCourse.findById(id);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered Course not found');
+  }
+
+  const semesterRegistation = isOfferedCourseExists.semesterRegistration;
+
+  const semesterRegistrationStatus =
+    await SemesterRegistration.findById(semesterRegistation).select('status');
+
+  if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `Offered course can not update ! because the semester ${semesterRegistrationStatus}`,
+    );
+  }
+
+  const result = await OfferedCourse.findByIdAndDelete(id);
+
+  return result;
+};
 
 export const OfferedCourseServices = {
   createOfferedCourseIntoDB,
--- Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ Express Expert and Mongoose Master-L2/Mastering Mongoose-M8/first-project/src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,18 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+//refactoring for clean code
+const timeStringSchema = z.string().refine(
+  (time) => {
+    // console.log(time); // will show send time
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23 //regex to format time
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format , expected "HH:MM" in 24 hours format',
+  },
+); // HH: MM   00-23: 00-59 | validation format
+
 // start time must be smaller than end time i.e 9-11 | both startTime and endTime are inside body so we can use refine in body
 const createOfferedCourseValidationSchema = z.object({
   body: z
@@ -13,26 +25,8 @@ const createOfferedCourseValidationSchema = z.object({
       section: z.number(),
       maxCapacity: z.number(),
       days: z.array(z.enum([...Days] as [string, ...string[]])), // must encapsulate inside array
-      startTime: z.string().refine(
-        (time) => {
-          // console.log(time); // will show send time
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23 //regex to format time
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format , expected "HH:MM" in 24 hours format',
-        },
-      ), // HH: MM   00-23: 00-59 | validation format
-      endTime: z.string().refine(
-        (time) => {
-          // console.log(time); // will show send time
-          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23 //regex to format time
-          return regex.test(time);
-        },
-        {
-          message: 'Invalid time format , expected "HH:MM" in 24 hours format',
-        },
-      ),
+      startTime: timeStringSchema,
+      endTime: timeStringSchema,
     })
     .refine(
       (body) => {
@@ -50,14 +44,31 @@ const createOfferedCourseValidationSchema = z.object({
       },
     ),
 });
+//removed optional bz we want user to provide every field to maintain proper update
 const updateOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    faculty: z.string().optional(),
-    maxCapacity: z.number().optional(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])).optional(),
-    startTime: z.string().optional(), // HH: MM   00-23: 00-59
-    endTime: z.string().optional(),
-  }),
+  body: z
+    .object({
+      faculty: z.string(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])), // must give array
+      startTime: timeStringSchema, // HH: MM   00-23: 00-59
+      endTime: timeStringSchema,
+    })
+    .refine(
+      (body) => {
+        //only comparing with
+        // startTime : 10:30  => 1970-01-01T10:30
+        //endTime : 12:30  =>  1970-01-01T12:30
+
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+
+        return end > start;
+      },
+      {
+        message: 'Start time should be before End time !  ',
+      },
+    ),
 });
 
 export const OfferedCourseValidations = {
